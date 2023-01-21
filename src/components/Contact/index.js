bc26f51fd7d4719cb88ddb75af0b86a7e1f16e3c
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-01-21 11:27:21 -0700

    Update contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,106 +1,31 @@
-import React, { useState } from 'react';
-import {
-  contact,
-  messageStatuses
-} from '../../constants/contact'
+import { contact } from '../../constants/contact'
 
 import {
   Container,
-  Form,
-  Header,
   HeaderTitle,
-  ErrorMessage,
-  HeaderButton,
-  FormButton,
+  ContactButton,
+  EmailButton,
+  ContactBtnsContainer,
 } from './styled'
 
-const Contact = () => {
-  const [ name, setName ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ message, setMessage ] = useState('');
-  const [ sentMessage, setSentMessage ] = useState(false);
-  const [ errorMessage, setErrorMessage ] = useState(false);
-  const validForm = (name && email && message) !== '' ? true : false;
-
-  const handleSubmit = (event) => {
-    if(validForm) {
-      const templateId = 'template_tbr3zii';
-      const serviceId = 'service_5r0kymp';
-      sendFeedback(serviceId, templateId, {message, name, email})
-    }
-  }
-  
-  const sendFeedback = (serviceId, templateId, variables) => {
-    window.emailjs.send(serviceId, templateId, variables).then(res => {
-      setSentMessage(true);
-    })
-    .catch(err => setErrorMessage(false));
-  }
-
-  return (
-    <Container>
-      <Header>
-        <div>
-          {contact.headers.map(header => <HeaderTitle>{header}</HeaderTitle>)}
-        </div>
-        <div>
-          <a href={contact.pdfHref}>
-            <HeaderButton>
-              <h3>{contact.resumeTitle}</h3>
-            </HeaderButton>
-          </a>
-        </div>
-      </Header>
-
-      <Form>
-        {(sentMessage && !errorMessage) && (
-          <>
-            {messageStatuses.sent.map(text => <h3>{text}</h3>)}
-          </>
-        )}
-        {(sentMessage && errorMessage) && (
-          <ErrorMessage>
-            {messageStatuses.error.map(text => <h3>{text}</h3>)}
-          </ErrorMessage>
-        )}
-        {(!sentMessage && !errorMessage) && (
-          <>
-            <div>
-              <p>Name</p>
-              <input 
-                type="text" 
-                value={name} 
-                onChange={(e) => setName(e.target.value)} 
-              />
-            </div>
-            <div>
-              <p>Email</p>
-              <input 
-                type="text" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div>
-              <p>Message</p>
-              <textarea 
-                rows="4" 
-                cols="15"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              >
-              </textarea>
-            </div>
-            <FormButton onClick={(e) => handleSubmit(e)}>
-              <h3>
-                Send message
-              </h3>
-            </FormButton>
-          </>
-        )}
-      </Form>
-    </Container>
-  );
-};
+const Contact = () => (
+  <Container>
+    <div>
+      {contact.headers.map(header => <HeaderTitle>{header}</HeaderTitle>)}
+    </div>
+    <ContactBtnsContainer>
+      <a href={contact.emailHref}>
+        <EmailButton color="green">
+          <h3>{contact.emailBtnText}</h3>
+        </EmailButton>
+      </a>
+      <a href={contact.pdfHref}>
+        <ContactButton>
+          <h3>{contact.resumeTitle}</h3>
+        </ContactButton>
+      </a>
+    </ContactBtnsContainer>
+  </Container>
+);
 
 export default Contact;
--- src/components/Contact/styled.js
+++ src/components/Contact/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 import { 
   btnPop, 
+  btnPopHome,
   fadeInBottom
 } from '../../constants/animations'
 
@@ -10,138 +11,29 @@ export const Container = styled.div`
   width: 1200px;
   max-width: 1200px;
   display: flex;
-  justify-content: center;
-  -webkit-animation: ${fadeInBottom} 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
-  animation: ${fadeInBottom} 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
-
-  & p {
-    font-weight: 500;
-    font-size: 0.7rem;
-    color: ${({ theme }) => theme.colors.yellow};
-  }
-
-  & textarea {
-    resize: none;
-  }
-
-  & a p:hover {
-    color: ${({ theme }) => theme.colors.blue}
-  }
-
-  @media screen and (max-width: 768px) {
-    justify-content: flex-start;
-    height: fit-content;
-    align-items: center;
-    margin: 0rem;
-    margin-top: 5rem;
-    flex-direction: column
-  }
-`
-
-export const Header = styled.div`
-  width: 50%;
-  display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: left;
-  padding-right: 3.5rem;
-  align-items: flex-end;
-
-  & > div:first-of-type {
-    margin-bottom: 2rem;
-  }
-
-  & div {
-    width: 11rem;
-  }
-
-  & a {
-    text-decoration: none;
-    color: black;
-  }
-
-  & h1 {
-    font-size: 1.5rem;
-  }
-
-  @media screen and (max-width: 768px) {
-    align-items: center;
-    width: 100%;
-    padding: 3rem 0rem;
-
-    & div {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-  }
-  }
+  align-items: center;
+  -webkit-animation: ${fadeInBottom} 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+  animation: ${fadeInBottom} 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
 `
 
 export const HeaderTitle = styled.h3`
+  text-align: center;
   font-size: 0.95rem;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.yellow};
 `
 
-export const Form = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-  padding-left: 1rem;
-
-  & p {
-    font-weight: 600;
-    color: ${({ theme }) => theme.colors.yellow};
-  }
-
-  & div {
-    margin: .7rem 0;
-  }
-
-  & textarea,
-  & input {
-    border: none;
-    border-left: ${({ theme }) => `3px solid ${theme.colors.green}`};
-    font-size: 0.7rem;
-    padding: 0.4rem;
-    width: 10rem;
-    color: rgba(255, 255, 255, 0.456);
-    background: #03ca9f1c;
-    margin-top: 0.2rem;
-    border-radius: 3px;
-    font-family: 'Source Sans Pro', sans-serif;
-  }
-
-  & textarea:focus,
-  & input:focus {
-    color: ${({ theme }) => theme.colors.white};
-  }
-
-  @media screen and (max-width: 768px) {
-    padding: 0rem;
-    align-items: center;
-    width: 100%;
-    margin-bottom: 3rem;
-  }
-`
-
-export const ErrorMessage = styled.div`
-  @media screen and (max-width: 768px) {
-    text-align: center;
-  }
-`
-
-export const HeaderButton = styled.div`
+export const ContactButton = styled.div`
   border-radius: 1rem; 
   padding:  0.4rem 0.7rem;
+  margin: 11px 22px;
   cursor: none;
   text-align: center;
   font-size: 0.6rem;
-  background: ${({ theme }) => theme.colors.yellow};
+  background: ${({ theme, color = 'yellow' }) => theme.colors[color]};
   color: black;
-  width: 5rem;
 
   &:hover {
     -webkit-animation: ${btnPop} 0.2s ease-in-out alternate both;
@@ -149,9 +41,18 @@ export const HeaderButton = styled.div`
   }
 `
 
-export const FormButton = styled(HeaderButton)`
-  margin-top: 2rem;
-  text-align: center;
-  width: 6rem;
-  background: ${({ theme }) => theme.colors.yellow};
+export const EmailButton = styled(ContactButton)`
+  &:hover {
+    -webkit-animation: ${btnPopHome} 0.2s ease-in-out alternate both;
+    animation: ${btnPopHome} 0.2s ease-in-out alternate both;
+  }
+`
+
+export const ContactBtnsContainer = styled.div`
+  display: flex;
+  margin-top: 100px;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `
